dc3f0355cbaa66df3cddfff5056faf91bf3870a7
TI2042 <tis3.1415@gmail.com>
2020-07-01 11:35:30 +0400

    Fix

--- SecuritySystemListImplement/Models/EquipmentDevice.cs
+++ SecuritySystemListImplement/Models/EquipmentDevice.cs
@@ -2,7 +2,7 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace SecuritySystemFileImplement.Models
+namespace SecuritySystemListImplement.Models
 {
     public class EquipmentDevice
     {
