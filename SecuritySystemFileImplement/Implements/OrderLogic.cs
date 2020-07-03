11942afaac8e495ef9e01cbfab422d2be2551bdc
TI2042 <tis3.1415@gmail.com>
2020-07-03 20:34:59 +0400

    Fix

--- SecuritySystemFileImplement/Implements/OrderLogic.cs
+++ SecuritySystemFileImplement/Implements/OrderLogic.cs
@@ -1,11 +1,11 @@
 ﻿using SecuritySystemFileImplement.Models;
 using SecuritySystemsBusinessLogic.BindingModels;
+using SecuritySystemsBusinessLogic.Enums;
 using SecuritySystemsBusinessLogic.Interfaces;
 using SecuritySystemsBusinessLogic.ViewModels;
 using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
 
 namespace SecuritySystemFileImplement.Implements
 {
@@ -25,7 +25,7 @@ namespace SecuritySystemFileImplement.Implements
             {
                 order = source.Orders.FirstOrDefault(rec => rec.Id == model.Id);
                 if (order == null)
-                    throw new Exception("Элемент не найден");               
+                    throw new Exception("Заказ не найден");
             }
             else
             {
@@ -34,8 +34,12 @@ namespace SecuritySystemFileImplement.Implements
                 source.Orders.Add(order);
             }
             order.EquipmentId = model.EquipmentId;
+            order.ClientFIO = model.ClientFIO;
+            order.ClientId = (int)model.ClientId;
             order.Count = model.Count;
             order.DateCreate = model.DateCreate;
+            order.ImplementerFIO = model.ImplementerFIO;
+            order.ImplementerId = model.ImplementerId;
             order.DateImplement = model.DateImplement;
             order.Status = model.Status;
             order.Sum = model.Sum;
@@ -50,28 +54,32 @@ namespace SecuritySystemFileImplement.Implements
             }
             else
             {
-                throw new Exception("Элемент не найден");
+                throw new Exception("Заказ не найден");
             }
         }
 
         public List<OrderViewModel> Read(OrderBindingModel model)
         {
             return source.Orders
-            .Where(rec => model == null
-                || rec.Id == model.Id
-                || model.DateFrom.HasValue && model.DateTo.HasValue && rec.DateCreate >= model.DateFrom && rec.DateCreate <= model.DateTo
-                || model.ClientId.HasValue && rec.ClientId == model.ClientId)
+            .Where(rec => model == null 
+            || model.Id.HasValue && rec.Id == model.Id && rec.ClientId == model.ClientId 
+            ||(model.DateTo.HasValue && model.DateFrom.HasValue && rec.DateCreate >= model.DateFrom && rec.DateCreate <= model.DateTo) 
+            || (model.ClientId.HasValue && rec.ClientId == model.ClientId)
+            ||(model.FreeOrder.HasValue && model.FreeOrder.Value && !(rec.ImplementerFIO != null)) 
+            ||(model.ImplementerId.HasValue && rec.ImplementerId == model.ImplementerId.Value && rec.Status == OrderStatus.Выполняется))
             .Select(rec => new OrderViewModel
             {
                 Id = rec.Id,
                 EquipmentId = rec.EquipmentId,
-                EquipmentName = source.Equipments.FirstOrDefault((r) => r.Id == rec.EquipmentId)?.EquipmentName,
+                EquipmentName = source.Equipments.FirstOrDefault((r) => r.Id == rec.EquipmentId).EquipmentName,
+                ClientFIO = rec.ClientFIO,
+                ClientId = rec.ClientId,
+                ImplementorId = rec.ImplementerId,
+                ImplementerFIO = !string.IsNullOrEmpty(rec.ImplementerFIO) ? rec.ImplementerFIO : string.Empty,
                 Count = rec.Count,
                 DateCreate = rec.DateCreate,
                 DateImplement = rec.DateImplement,
                 Status = rec.Status,
-                ClientFIO = rec.ClientFIO,
-                ClientId = rec.ClientId,
                 Sum = rec.Sum
             }).ToList();
         }
