9ab56c10e0ad42ca2b57ef910cc679b0f5a1ad91
TI2042 <55748500+TI2042@users.noreply.github.com>
2020-08-29 14:05:54 +0400

    Fix models

--- SecuritySystemFileImplement/Models/Implementer.cs
+++ SecuritySystemFileImplement/Models/Implementer.cs
@@ -8,5 +8,7 @@ namespace SecuritySystemFileImplement.Models
     {
         public int Id { set; get; }
         public string ImplementerFIO { set; get; }
+        public int WorkingTime { get; set; }
+        public int PauseTime { get; set; }
     }
 }
